11b758d47e3a76dd45b0270afab643f58758949f
MarcusMohns <92151780+MarcusMohns@users.noreply.github.com>
2022-06-19 16:23:41 +0200

    added and fixed toast example

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
+
 const helloWorld = () => {
     console.log("hellkoooo")
+}
+
+
+const showToast = () => {
+
+    const toastLiveExample = document.getElementById('liveToast')
+    const toast = new bootstrap.Toast(toastLiveExample)
+    toast.show()
+
 }
