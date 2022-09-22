6647eeaa1c9ec2c2f73e9844aec0134890f6bc51
MarcusMohns <92151780+MarcusMohns@users.noreply.github.com>
2022-09-22 23:40:27 +0200

    lade till status-bar

--- script.js
+++ script.js
@@ -1,13 +1,5 @@
-
-const helloWorld = () => {
-    console.log("hellkoooo")
-}
-
-
 const showToast = () => {
-
-    const toastLiveExample = document.getElementById('liveToast')
-    const toast = new bootstrap.Toast(toastLiveExample)
-    toast.show()
-
-}
+  const toastLiveExample = document.getElementById("liveToast");
+  const toast = new bootstrap.Toast(toastLiveExample);
+  toast.show();
+};
