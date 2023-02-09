cd85fa5ec6cb9a90440e4ffa7d34dd0b9e32c148
Marcus Mohns <92151780+MarcusMohns@users.noreply.github.com>
2023-02-09 14:27:15 +0100

    animation spelar mer än 1 gång

--- script.js
+++ script.js
@@ -13,9 +13,10 @@ const startBtn = (value) => {
 
 const observer = new IntersectionObserver((entries) => {
   entries.forEach((entry) => {
-    console.log(entry);
     if (entry.isIntersecting) {
       entry.target.classList.add("show");
+    } else {
+      entry.target.classList.remove("show");
     }
   });
 });
